e95f70227a841fdf95e5438e19b389c2993cab29
tr15tan01 <t.varamashvili@gmail.com>
2022-04-10 00:18:02 +0400

    app.js route play

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
             {/* <Route path="/mainpage" element={!this.state.currentUser ? (<Navigate to="/signin" />) : (<MainPage />)} /> */}
             <Route path="/hrsaas/mainpage" element={this.state.currentUser ? (<MainPage />) : (<Navigate to="/signin" />)} />
             {/* <Route path="hrsaas/signin" element={this.state.currentUser ? (<Navigate to="/hrsaas/mainpage" />) : (<SignInPage />)} /> */}
-            <Route path="hrsaas/signin" element={<SignInPage />} />
+            <Route path="/hrsaas/signin" element={<SignInPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             {/* <Route path="/signup" element={this.state.currentUser ? (<Navigate to="/" />) : (<SignUpPage />)} /> */}
             <Route path="/pricing" element={<PricingPage />} />
